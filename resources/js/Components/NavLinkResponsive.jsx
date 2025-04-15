790b70a37f6ea9d8d05a65b08ee0b267f59dbc90
Satria Yudha <satriayuda2017@gmail.com>
2025-04-15 23:36:42 +0800

    create sidebar

--- resources/js/Components/NavLinkResponsive.jsx
+++ resources/js/Components/NavLinkResponsive.jsx
@@ -10,7 +10,7 @@ export default function NavLinkResponsive({ active = false, url = '#', title, ic
                 active
                     ? 'bg-gradient-to-r from-orange-400 via-orange-500 to-orange-500 font-semibold text-white hover:text-white'
                     : 'text-muted-foreground hover:text-orange-500',
-                'flex items-center gap-3 rounded-lg p-2 font-medium transition-all',
+                'flex items-center gap-3 rounded-lg p-2 font-medium transition-all p-3',
             )}
         >
             <Icon className="h-4 w-4" />
--- resources/js/Layouts/AppLayout.jsx
+++ resources/js/Layouts/AppLayout.jsx
@@ -1,8 +1,17 @@
 import ApplicationLogo from '@/Components/ApplicationLogo';
+import NavLinkResponsive from '@/Components/NavLinkResponsive';
 import { Avatar, AvatarFallback } from '@/Components/ui/avatar';
 import { Button } from '@/Components/ui/button';
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from '@/Components/ui/dropdown-menu';
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from '@/Components/ui/dropdown-menu';
 import { Head, Link } from '@inertiajs/react';
+import { IconAlertCircle, IconBook, IconBooks, IconBuildingCommunity, IconCategory, IconChartDots2, IconCircleKey, IconCreditCardPay, IconCreditCardRefund, IconDashboard, IconKeyframe, IconLayoutKanban, IconLogout, IconMoneybag, IconRoute, IconSettingsExclamation, IconStack3, IconUser, IconUsersGroup, IconVersions } from '@tabler/icons-react';
 import { Toaster } from 'sonner';
 
 export default function AppLayout({ title, children }) {
@@ -16,7 +25,40 @@ export default function AppLayout({ title, children }) {
                         <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
                             <ApplicationLogo />
                         </div>
-                        <div className="flex-1">{/*sidebar*/}</div>
+                        <div className="flex-1">
+                            {/*sidebar*/}
+                            <nav className='grid items-start px-2 text-sm font-semibold lg:px-4'>
+                                <div className='px-3 py-2 text-sm font-semibold text-foreground'>Dashboard</div>
+                                <NavLinkResponsive url='#' title="Dashboard" icon={IconDashboard}/>
+
+                                <div className='px-3 py-2 text-sm font-semibold text-foreground'>Statistik</div>
+                                <NavLinkResponsive url='#' title="Statistik Peminjaman" icon={IconChartDots2}/>
+                                <NavLinkResponsive url='#' title="Laporan Denda" icon={IconMoneybag}/>
+                                <NavLinkResponsive url='#' title="Laporan Stok Buku" icon={IconStack3}/>
+                                <div className='px-3 py-2 text-sm font-semibold text-foreground'>Master</div>
+                                <NavLinkResponsive url='#' title="Kategori" icon={IconCategory}/>
+                                <NavLinkResponsive url='#' title="Penerbit" icon={IconBuildingCommunity}/>
+                                <NavLinkResponsive url='#' title="Buku" icon={IconBooks}/>
+                                <NavLinkResponsive url='#' title="Pengguna" icon={IconUsersGroup}/>
+                                <NavLinkResponsive url='#' title="Pengaturan Denda" icon={IconSettingsExclamation}/>
+
+                                <div className='px-3 py-2 text-sm font-semibold text-foreground'>Peran dan Izin</div>
+                                <NavLinkResponsive url='#' title="Peran" icon={IconCircleKey}/>
+                                <NavLinkResponsive url='#' title="Izin" icon={IconVersions}/>
+                                <NavLinkResponsive url='#' title="Tetapkan Izin" icon={IconKeyframe}/>
+                                <NavLinkResponsive url='#' title="Tetapkan Peran" icon={IconLayoutKanban}/>
+                                <NavLinkResponsive url='#' title="Akses Rute" icon={IconRoute}/>
+
+                                <div className='px-3 py-2 text-sm font-semibold text-foreground'>Transaksi</div>
+                                <NavLinkResponsive url='#' title="Peminjaman" icon={IconCreditCardPay}/>
+                                <NavLinkResponsive url='#' title="Pengembalian" icon={IconCreditCardRefund}/>
+
+                                <div className='px-3 py-2 text-sm font-semibold text-foreground'>Lainnya</div>
+                                <NavLinkResponsive url='#' title="Pengumuman" icon={IconAlertCircle}/>
+                                <NavLinkResponsive url={route('profile.edit')} title="Profile" icon={IconUser}/>
+                                <NavLinkResponsive url='#' title="LogOut" icon={IconLogout}/>
+                            </nav>
+                        </div>
                     </div>
                 </div>
                 <div className="flex w-full flex-col lg:w-4/5">
@@ -34,12 +76,10 @@ export default function AppLayout({ title, children }) {
                             </DropdownMenuTrigger>
                             <DropdownMenuContent align="end">
                                 <DropdownMenuLabel>Akun Saya</DropdownMenuLabel>
-                                <DropdownMenuSeparator/>
+                                <DropdownMenuSeparator />
                                 <DropdownMenuItem>Profile</DropdownMenuItem>
                                 <DropdownMenuItem asChild>
-                                    <Link href="#">
-                                        Logout
-                                    </Link>
+                                    <Link href="#">Logout</Link>
                                 </DropdownMenuItem>
                             </DropdownMenuContent>
                         </DropdownMenu>
