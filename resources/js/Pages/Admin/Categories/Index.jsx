3bd4de746407ff1b9879d3afff739c9f093f1a6c
Satria Yudha <satriayuda2017@gmail.com>
2025-05-30 01:31:55 +0800

    pagination and seaching on category

--- resources/js/Pages/Admin/Categories/Index.jsx
+++ resources/js/Pages/Admin/Categories/Index.jsx
@@ -2,15 +2,28 @@ import HeaderTitle from "@/Components/HeaderTitle";
 import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "@/Components/ui/alert-dialog";
 import { Avatar, AvatarFallback, AvatarImage } from "@/Components/ui/avatar";
 import { Button } from "@/Components/ui/button";
-import { Card, CardContent } from "@/Components/ui/card";
+import { Card, CardContent, CardFooter, CardHeader } from "@/Components/ui/card";
+import { Input } from "@/Components/ui/input";
+import { Pagination, PaginationContent, PaginationItem, PaginationLink } from "@/Components/ui/pagination";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/Components/ui/table";
+import { UseFilter } from "@/hooks/useFilter";
 import AppLayout from "@/Layouts/AppLayout";
 import { flashMessage } from "@/lib/utils";
 import { Link, router } from "@inertiajs/react";
 import { IconCategory, IconPencil, IconPlus, IconTrash } from "@tabler/icons-react";
+import { useState } from "react";
 import { toast } from "sonner";
 
 export default function Index(props){
+    const {data: categories, meta} = props.categories
+    const [params, setParams] = useState(props.state)
+
+    UseFilter ({
+        route: route('admin.categories.index'),
+        values: params,
+        only: ['categories'],
+    });
+
     return (
         <div className="flex flex-col w-full pb-32">
             <div className="flex flex-col items-start justify-between mb-8 gap-y-4 lg:flex-row lg:items-center">
@@ -32,6 +45,16 @@ export default function Index(props){
             </div>
 
             <Card>
+                <CardHeader>
+                    <div className="flex w-full flex-col gap-4 lg:flex-row lg:items-center">
+                        <Input
+                            className="w-full sm:w-1/4"
+                            placeholder="Search..."
+                            value={params?.search}
+                            onChange={(e) => setParams((prev) => ({...prev, search: e.target.value}))}
+                        />
+                    </div>
+                </CardHeader>
                 <CardContent className="px-0 py-0 [&-td]:whitespace-nowrap [&_td]:px-6 [&_th]:px-6">
                     <Table className='w-full'>
                         <TableHeader>
@@ -45,13 +68,13 @@ export default function Index(props){
                             </TableRow>
                         </TableHeader>
                         <TableBody>
-    {props.categories.map((category, index) => {
+    {categories.map((category, index) => {
         // Menambahkan log untuk memeriksa nilai category.cover
         console.log(category.cover);  // Memeriksa URL gambar
 
         return (
             <TableRow key={index}>
-                <TableCell>{ index + 1 }</TableCell>
+                <TableCell>{ index + 1 + (meta.current_page - 1) * meta.per_page}</TableCell>
                 <TableCell>{category.name}</TableCell>
                 <TableCell>{category.slug}</TableCell>
                 <TableCell>
@@ -110,6 +133,29 @@ export default function Index(props){
 
                     </Table>
                 </CardContent>
+                <CardFooter className="flex flex-col items-center justify-between w-full py-2 border-t lg:flex-row">
+                    <p className="mb-2 text-sm text-muted-foreground">
+                    Menampilkan <span className="font-medium text-orange-500">{meta.from ?? 0}</span> dari {meta.total} kategori
+                    </p>
+                    <div className="overflow-x-auto">
+                        {meta.has_pages && (
+                            <Pagination>
+                                <PaginationContent className="flex flex-wrap justify-center lg:justify-end">
+                                    {meta.links.map((link, index) => (
+                                        <PaginationItem key={index} className="mx-1 mb-1 lb:mb-0">
+                                            <PaginationLink
+                                                href={link.url}
+                                                isActive={link.active}
+                                            >
+                                                {link.label}
+                                            </PaginationLink>
+                                        </PaginationItem>
+                                    ))}
+                                </PaginationContent>
+                            </Pagination>
+                        )}
+                    </div>
+                </CardFooter>
             </Card>
 
         </div>
